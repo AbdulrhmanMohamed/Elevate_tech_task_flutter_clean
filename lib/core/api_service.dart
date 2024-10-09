468ab1aea194b8175dd9f4e99663493b7c99664b
AbdulrhmanMohamed <afarg843@gmail.com>
2024-10-09 04:13:04 +0200

    adding apiService with dio

---
+++ lib/core/api_service.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio _dio ;
+  String baseUrl="https://fakestoreapi.com";
+  ApiService({required Dio dio }) : _dio = dio;
+
+ Future< Map<String,dynamic>> get(String endpoint)async{
+    var response=await this._dio.get("$baseUrl$endpoint");
+    return response.data;
+  }
+
+
+
+}
